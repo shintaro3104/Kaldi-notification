84f7bc025924288660cba9c7b8f28b0e12d4820f
shintaro3104 <shintaro.kishino@gmail.com>
2026-04-25 22:27:54 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ from bs4 import BeautifulSoup
 # --- Configuration & Constants ---
 BASE_URL = "https://map.kaldi.co.jp/kaldi/articleList"
 DB_FILE = "seen.db"
-KEYWORDS = ["浦和", "赤羽", "川口", "レイクタウン", "与野", "戸田", "銀座"]
+KEYWORDS = ["浦和", "レイクタウン", "与野", "戸田", "北海道"]
 HEADLINE = "☕️ KALDIの新着セール情報が届いたよ！\n\n"
 
 # CSS Selectors (Centralized for easy updates)
